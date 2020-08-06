4dd47633ec3b1c821119d76eaff9f23b7fb64277
Claire Tregunna <c.tregunna1980@gmail.com>
2020-08-06 13:22:11 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,12 +3,21 @@ const mainNav = document.querySelector('.mainNav');
 const menuClose = document.querySelector('#menuClose');
 
 mobileMenu.addEventListener('click', e => {
+    window.onresize = function() {
+        if (window.innerWidth > 768) {
+          menuIcon.style.display = 'none';
+        } else {
+          menuIcon.style.display = 'block';
+        }
+      }
+
     mainNav.style.display = 'block';
     mobileMenu.style.display = 'none';
     menuClose.style.display = 'block';
 });
 
 menuClose.addEventListener('click', e => {
+
     mainNav.style.display = 'none';
     mobileMenu.style.display = 'block';
     menuClose.style.display = 'none';
