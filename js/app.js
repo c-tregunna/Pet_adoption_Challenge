505dafeeae5d57ca082e5a40e9bbca8e4ddff5d2
Claire Tregunna <c.tregunna1980@gmail.com>
2020-08-07 12:23:50 +0100

    Mobile navigation refactor

--- js/app.js
+++ js/app.js
@@ -1,24 +1,6 @@
 const mobileMenu = document.querySelector('#menuIcon');
 const mainNav = document.querySelector('.mainNav');
-const menuClose = document.querySelector('#menuClose');
 
-mobileMenu.addEventListener('click', e => {
-    window.onresize = function() {
-        if (window.innerWidth > 768) {
-          menuIcon.style.display = 'none';
-        } else {
-          menuIcon.style.display = 'block';
-        }
-      }
-
-    mainNav.style.display = 'block';
-    mobileMenu.style.display = 'none';
-    menuClose.style.display = 'block';
-});
-
-menuClose.addEventListener('click', e => {
-
-    mainNav.style.display = 'none';
-    mobileMenu.style.display = 'block';
-    menuClose.style.display = 'none';
-});
+mobileMenu.addEventListener('click', () => {
+    mainNav.classList.toggle('hidden');
+ });
